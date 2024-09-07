30e6f630998cbfa27ee7d0cf67ca3176727d73d0
rajveerkhosa <rajveerk23@icloud.com>
2024-09-07 00:51:03 -0700

    Fixed Basic Logic

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,3 @@
-//
 //modified by: Rajveer Khosa
 //date: 08/30/24
 //
@@ -26,40 +25,40 @@ using namespace std;
 
 class Global {
 public:
-	int xres, yres;
-	float w; 
-	float vel; 
-	float pos[2];
+    int xres, yres;
+    float w;
+    float vel;
+    float pos[2];
 
-	//	Flag to Check if Window was Resized
-	bool ean; 
+    //  Flag to Check if Window was Resized
+    bool ean;
 
-	Global() {
-	    xres = 400;
-	    yres = 200;
+    Global() {
+        xres = 400;
+        yres = 200;
         w = 20.0f;
-        vel = 30.0f; 
-        pos[0] = 0.0f + w; 
-        pos[1] = yres / 2.0f; 
+        vel = 30.0f;
+        pos[0] = 0.0f + w;
+        pos[1] = yres / 2.0f;
     }
 } g;
 
 class X11_wrapper {
 private:
-	Display *dpy;
-	Window win;
-	GLXContext glc;
+    Display *dpy;
+    Window win;
+    GLXContext glc;
 public:
-	~X11_wrapper();
-	X11_wrapper();
-	void set_title();
-	bool getXPending();
-	XEvent getXNextEvent();
-	void swapBuffers();
-	void reshape_window(int width, int height);
-	void check_resize(XEvent *e);
-	void check_mouse(XEvent *e);
-	int check_keys(XEvent *e);
+    ~X11_wrapper();
+    X11_wrapper();
+    void set_title();
+    bool getXPending();
+    XEvent getXNextEvent();
+    void swapBuffers();
+    void reshape_window(int width, int height);
+    void check_resize(XEvent *e);
+    void check_mouse(XEvent *e);
+    int check_keys(XEvent *e);
 } x11;
 
 //Function prototypes
@@ -70,251 +69,281 @@ void render(void);
 
 int main()
 {
-	init_opengl();
-	int done = 0;
-	//main game loop
-	while (!done) {
-		//look for external events such as keyboard, mouse.
-		while (x11.getXPending()) {
-			XEvent e = x11.getXNextEvent();
-			x11.check_resize(&e);
-			x11.check_mouse(&e);
-			done = x11.check_keys(&e);
-		}
-		physics();
-		render();
-		x11.swapBuffers();
-		usleep(200);
-	}
-	return 0;
+    init_opengl();
+    int done = 0;
+    //main game loop
+    while (!done) {
+        //look for external events such as keyboard, mouse.
+        while (x11.getXPending()) {
+            XEvent e = x11.getXNextEvent();
+            x11.check_resize(&e);
+            x11.check_mouse(&e);
+            done = x11.check_keys(&e);
+        }
+        physics();
+        render();
+        x11.swapBuffers();
+        usleep(200);
+    }
+    return 0;
 }
 
 
 X11_wrapper::~X11_wrapper()
 {
-	XDestroyWindow(dpy, win);
-	XCloseDisplay(dpy);
+    XDestroyWindow(dpy, win);
+    XCloseDisplay(dpy);
 }
 
 X11_wrapper::X11_wrapper()
 {
-	GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-	int w = g.xres, h = g.yres;
-	dpy = XOpenDisplay(NULL);
-	if (dpy == NULL) {
-		cout << "\n\tcannot connect to X server\n" << endl;
-		exit(EXIT_FAILURE);
-	}
-	Window root = DefaultRootWindow(dpy);
-	XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
-	if (vi == NULL) {
-		cout << "\n\tno appropriate visual found\n" << endl;
-		exit(EXIT_FAILURE);
-	} 
-	Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
-	XSetWindowAttributes swa;
-	swa.colormap = cmap;
-	swa.event_mask =
-		ExposureMask | KeyPressMask | KeyReleaseMask |
-		ButtonPress | ButtonReleaseMask |
-		PointerMotionMask |
-		StructureNotifyMask | SubstructureNotifyMask;
-	win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
-		InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
-	set_title();
-	glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
-	glXMakeCurrent(dpy, win, glc);
+    GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+    int w = g.xres, h = g.yres;
+    dpy = XOpenDisplay(NULL);
+    if (dpy == NULL) {
+        cout << "\n\tcannot connect to X server\n" << endl;
+        exit(EXIT_FAILURE);
+    }
+    Window root = DefaultRootWindow(dpy);
+    XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
+    if (vi == NULL) {
+        cout << "\n\tno appropriate visual found\n" << endl;
+        exit(EXIT_FAILURE);
+    }
+    Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+    XSetWindowAttributes swa;
+    swa.colormap = cmap;
+    swa.event_mask =
+        ExposureMask | KeyPressMask | KeyReleaseMask |
+        ButtonPress | ButtonReleaseMask |
+        PointerMotionMask |
+        StructureNotifyMask | SubstructureNotifyMask;
+    win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
+        InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+    set_title();
+    glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+    glXMakeCurrent(dpy, win, glc);
 }
 
 void X11_wrapper::set_title()
 {
-	//Set the window title bar.
-	XMapWindow(dpy, win);
-	XStoreName(dpy, win, "3350 Lab-1");
+    //Set the window title bar.
+    XMapWindow(dpy, win);
+    XStoreName(dpy, win, "3350 Lab-1");
 }
 
 bool X11_wrapper::getXPending()
 {
-	//See if there are pending events.
-	return XPending(dpy);
+    //See if there are pending events.
+    return XPending(dpy);
 }
 
 XEvent X11_wrapper::getXNextEvent()
 {
-	//Get a pending event.
-	XEvent e;
-	XNextEvent(dpy, &e);
-	return e;
+    //Get a pending event.
+    XEvent e;
+    XNextEvent(dpy, &e);
+    return e;
 }
 
 void X11_wrapper::swapBuffers()
 {
-	glXSwapBuffers(dpy, win);
+    glXSwapBuffers(dpy, win);
 }
 
 void X11_wrapper::reshape_window(int width, int height)
 {
-	//Window has been resized.
-	g.xres = width;
-	g.yres = height;
-	//
-	glViewport(0, 0, (GLint)width, (GLint)height);
-	glMatrixMode(GL_PROJECTION); glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-	glOrtho(0, g.xres, 0, g.yres, -1, 1);
+    //Window has been resized.
+    g.xres = width;
+    g.yres = height;
+    //
+    glViewport(0, 0, (GLint)width, (GLint)height);
+    glMatrixMode(GL_PROJECTION); glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+    glOrtho(0, g.xres, 0, g.yres, -1, 1);
 }
 
 void X11_wrapper::check_resize(XEvent *e)
 {
-	//The ConfigureNotify is sent by the
-	//server if the window is resized.
-	if (e->type != ConfigureNotify)
-		return;
-	XConfigureEvent xce = e->xconfigure;
-	if (xce.width != g.xres || xce.height != g.yres) {
-		//Window size did change.
-		reshape_window(xce.width, xce.height);
+    //The ConfigureNotify is sent by the
+    //server if the window is resized.
+    if (e->type != ConfigureNotify)
+        return;
+    XConfigureEvent xce = e->xconfigure;
+    if (xce.width != g.xres || xce.height != g.yres) {
+        //Window size did change.
+        reshape_window(xce.width, xce.height);
 
 
-		//	Sets the flag to true
-		// 	if the Window was Resized
-		g.ean = true; 
-	}
-	else {
-		g.ean = false; 
-	}
-	
+        //  Sets the flag to true
+        //  if the Window was Resized
+        g.ean = true;
+    }
+    else {
+        g.ean = false;
+    }
+
 }
 //-----------------------------------------------------------------------------
 
 void X11_wrapper::check_mouse(XEvent *e)
 {
-	static int savex = 0;
-	static int savey = 0;
+    static int savex = 0;
+    static int savey = 0;
 
-	//Weed out non-mouse events
-	if (e->type != ButtonRelease &&
-		e->type != ButtonPress &&
-		e->type != MotionNotify) {
-		//This is not a mouse event that we care about.
-		return;
-	}
-	//
-	if (e->type == ButtonRelease) {
-		return;
-	}
-	if (e->type == ButtonPress) {
-		if (e->xbutton.button==1) {
-			//Left button was pressed.
-			//int y = g.yres - e->xbutton.y;
-			return;
-		}
-		if (e->xbutton.button==3) {
-			//Right button was pressed.
-			return;
-		}
-	}
-	if (e->type == MotionNotify) {
-		//The mouse moved!
-		if (savex != e->xbutton.x || savey != e->xbutton.y) {
-			savex = e->xbutton.x;
-			savey = e->xbutton.y;
-			//Code placed here will execute whenever the mouse moves.
+    //Weed out non-mouse events
+    if (e->type != ButtonRelease &&
+        e->type != ButtonPress &&
+        e->type != MotionNotify) {
+        //This is not a mouse event that we care about.
+        return;
+    }
+    //
+    if (e->type == ButtonRelease) {
+        return;
+    }
+    if (e->type == ButtonPress) {
+        if (e->xbutton.button==1) {
+            //Left button was pressed.
+            //int y = g.yres - e->xbutton.y;
+            return;
+        }
+        if (e->xbutton.button==3) {
+            //Right button was pressed.
+            return;
+        }
+    }
+    if (e->type == MotionNotify) {
+        //The mouse moved!
+        if (savex != e->xbutton.x || savey != e->xbutton.y) {
+            savex = e->xbutton.x;
+            savey = e->xbutton.y;
+            //Code placed here will execute whenever the mouse moves.
 
 
-		}
-	}
+        }
+    }
 }
 
 int X11_wrapper::check_keys(XEvent *e)
 {
-	if (e->type != KeyPress && e->type != KeyRelease)
-		return 0;
-	int key = XLookupKeysym(&e->xkey, 0);
-	if (e->type == KeyPress) {
-		switch (key) {
-			case XK_a:
-				//the 'a' key was pressed
-				break;
-			case XK_Escape:
-				//Escape key was pressed
-				return 1;
-		}
-	}
-	return 0;
+    if (e->type != KeyPress && e->type != KeyRelease)
+        return 0;
+    int key = XLookupKeysym(&e->xkey, 0);
+    if (e->type == KeyPress) {
+        switch (key) {
+            case XK_a:
+                //the 'a' key was pressed
+                break;
+            case XK_Escape:
+                //Escape key was pressed
+                return 1;
+        }
+    }
+    return 0;
 }
 
 void init_opengl(void)
 {
-	//OpenGL initialization
-	glViewport(0, 0, g.xres, g.yres);
-	//Initialize matrices
-	glMatrixMode(GL_PROJECTION); 
+    //OpenGL initialization
+    glViewport(0, 0, g.xres, g.yres);
+    //Initialize matrices
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-	//Set 2D mode (no perspective)
-	glOrtho(0, g.xres, 0, g.yres, -1, 1);
-	//
+    //Set 2D mode (no perspective)
+    glOrtho(0, g.xres, 0, g.yres, -1, 1);
+    //
     //initialize the model view matrix 
-    glMatrixMode(GL_MODELVIEW); 
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-	//Set the screen background color
+    //Set the screen background color
     //set when screen is clear
-	glClearColor(0.1, 0.1, 0.1, 1.0);
+    glClearColor(0.1, 0.1, 0.1, 1.0);
 }
 
 void physics()
 {
-	//No physics yet.
-	g.pos[0] += g.vel;
-	if (g.pos[0] >= (g.xres-g.w)) {
-		g.pos[0] = (g.xres-g.w);
-		g.vel = -g.vel;
-	}
-	if (g.pos[0] <= g.w) {
-		g.pos[0] = g.w;
-		g.vel = -g.vel;
-	}
+    //No physics yet.
+    g.pos[0] += g.vel;
+    if (g.pos[0] >= (g.xres-g.w)) {
+        g.pos[0] = (g.xres-g.w);
+        g.vel = -g.vel;
+    }
+    if (g.pos[0] <= g.w) {
+        g.pos[0] = g.w;
+        g.vel = -g.vel;
+    }
 }
+/*
+void render()
+{
+    //
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // If the Window Becomes Smaller than the Box
+    // Delete the Box
+    if (g.xres < 40) {
+        glColor3ub(255, 255, 255); 
+    }
+    else {
+        glColor3ub(255, 0, 0);
+    }
+
+    // If the window is smaller than 400
+    // Make the Box Red
+    if (g.xres < 400) {
+        glColor3ub(255, 0, 0);
+    }
+    else {
+        glColor3ub(100, 120, 220);
+    }
+
+    //draw the box
+    glPushMatrix();
+    glTranslatef(g.pos[0], g.pos[1], 0.0f);
+    glBegin(GL_QUADS);
+        glVertex2f(-g.w, -g.w);
+        glVertex2f(-g.w,  g.w);
+        glVertex2f( g.w,  g.w);
+        glVertex2f( g.w, -g.w);
+    glEnd();
+    glPopMatrix();
+
+    //  Change the Color 
+}
+*/
 
 void render()
 {
-	//
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	// If the Window Becomes Smaller than the Box
-	// Delete the Box
-	if (g.xres < 40) {
-		glColor3ub(255, 255, 255); 
-	}
-	else {
-		glColor3ub(255, 0, 0);
-	}
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	// If the window is smaller than 400
-	// Make the Box Red
-	if (g.xres < 400) {
-		glColor3ub(255, 0, 0);
-	}
-	else {
-		glColor3ub(100, 120, 220);
-	}
+    // If the window width is less than 40, don't draw the box
+    if (g.xres < 40) {
+        glColor3ub(30, 30, 30);
+    }
+    // Set the box color red if < 400
+    else if (g.xres < 400) {
+        glColor3ub(255, 0, 0);
+    }
+    // Set the box color blue
+    else {
+        glColor3ub(100, 120, 220); // regular color
+    }
 
-	//draw the box
-	glPushMatrix();
-	glTranslatef(g.pos[0], g.pos[1], 0.0f);
-	glBegin(GL_QUADS);
-		glVertex2f(-g.w, -g.w);
-		glVertex2f(-g.w,  g.w);
-		glVertex2f( g.w,  g.w);
-		glVertex2f( g.w, -g.w);
-	glEnd();
-	glPopMatrix();
+        // Draw the box
+        glPushMatrix();
+        glTranslatef(g.pos[0], g.pos[1], 0.0f);
+        glBegin(GL_QUADS);
+            glVertex2f(-g.w, -g.w);
+            glVertex2f(-g.w,  g.w);
+            glVertex2f( g.w,  g.w);
+            glVertex2f( g.w, -g.w);
+        glEnd();
+        glPopMatrix();
 
-	//	Change the Color 
+    // If the window is too small, nothing will be drawn
 }
-
-
-
 
 
 
